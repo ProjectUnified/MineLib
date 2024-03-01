ef9be15a862f4ea6eb660e8ffeda79014a8799eb
HSGamer <huynhqtienvtag@gmail.com>
2024-03-01 09:29:40 +0700

    use common Scheduler in GlobalScheduler

--- scheduler/global/src/main/java/io/github/projectunified/minelib/scheduler/global/GlobalScheduler.java
+++ scheduler/global/src/main/java/io/github/projectunified/minelib/scheduler/global/GlobalScheduler.java
@@ -1,16 +1,11 @@
 package io.github.projectunified.minelib.scheduler.global;
 
 import io.github.projectunified.minelib.scheduler.common.provider.ObjectProvider;
-import io.github.projectunified.minelib.scheduler.common.task.Task;
-import io.github.projectunified.minelib.scheduler.common.time.TaskTime;
-import io.github.projectunified.minelib.scheduler.common.time.TimerTaskTime;
+import io.github.projectunified.minelib.scheduler.common.scheduler.Scheduler;
 import io.github.projectunified.minelib.scheduler.common.util.Platform;
 import org.bukkit.plugin.Plugin;
 
-import java.util.function.BooleanSupplier;
-
-public interface GlobalScheduler {
-    ObjectProvider<GlobalScheduler> PROVIDER = new ObjectProvider<>(
+public interface GlobalScheduler extends Scheduler {
     ObjectProvider<Plugin, GlobalScheduler> PROVIDER = new ObjectProvider<>(
             ObjectProvider.entry(Platform.FOLIA::isPlatform, FoliaGlobalScheduler::new),
             ObjectProvider.entry(BukkitGlobalScheduler::new)
@@ -18,18 +13,5 @@ public interface GlobalScheduler {
 
     static GlobalScheduler get(Plugin plugin) {
         return PROVIDER.get(plugin);
-    }
-
-    Task run(Runnable runnable);
-
-    Task runLater(Runnable runnable, TaskTime delay);
-
-    Task runTimer(BooleanSupplier runnable, TimerTaskTime timerTaskTime);
-
-    default Task runTimer(Runnable runnable, TimerTaskTime timerTaskTime) {
-        return runTimer(() -> {
-            runnable.run();
-            return true;
-        }, timerTaskTime);
     }
 }
