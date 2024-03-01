0816a9404582c5a2e61120b79dd08c9e26ba7234
HSGamer <huynhqtienvtag@gmail.com>
2024-03-01 09:29:34 +0700

    use common Scheduler in AsyncScheduler

--- scheduler/async/src/main/java/io/github/projectunified/minelib/scheduler/async/AsyncScheduler.java
+++ scheduler/async/src/main/java/io/github/projectunified/minelib/scheduler/async/AsyncScheduler.java
@@ -1,16 +1,11 @@
 package io.github.projectunified.minelib.scheduler.async;
 
 import io.github.projectunified.minelib.scheduler.common.provider.ObjectProvider;
-import io.github.projectunified.minelib.scheduler.common.task.Task;
-import io.github.projectunified.minelib.scheduler.common.time.TaskTime;
-import io.github.projectunified.minelib.scheduler.common.time.TimerTaskTime;
+import io.github.projectunified.minelib.scheduler.common.scheduler.Scheduler;
 import io.github.projectunified.minelib.scheduler.common.util.Platform;
 import org.bukkit.plugin.Plugin;
 
-import java.util.function.BooleanSupplier;
-
-public interface AsyncScheduler {
-    ObjectProvider<AsyncScheduler> PROVIDER = new ObjectProvider<>(
+public interface AsyncScheduler extends Scheduler {
     ObjectProvider<Plugin, AsyncScheduler> PROVIDER = new ObjectProvider<>(
             ObjectProvider.entry(Platform.FOLIA::isPlatform, FoliaAsyncScheduler::new),
             ObjectProvider.entry(BukkitAsyncScheduler::new)
@@ -18,18 +13,5 @@ public interface AsyncScheduler {
 
     static AsyncScheduler get(Plugin plugin) {
         return PROVIDER.get(plugin);
-    }
-
-    Task run(Runnable runnable);
-
-    Task runLater(Runnable runnable, TaskTime delay);
-
-    Task runTimer(BooleanSupplier runnable, TimerTaskTime timerTaskTime);
-
-    default Task runTimer(Runnable runnable, TimerTaskTime timerTaskTime) {
-        return runTimer(() -> {
-            runnable.run();
-            return true;
-        }, timerTaskTime);
     }
 }
