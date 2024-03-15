903566805bba360a6a042bc8f4be8412ed39eab3
HSGamer <huynhqtienvtag@gmail.com>
2024-03-15 11:44:53 +0700

    add util-key

---
+++ util/util-key/src/main/java/io/github/projectunified/minelib/util/key/MinecraftKeyManager.java
@@ -0,0 +1,13 @@
+package io.github.projectunified.minelib.util.key;
+
+import org.bukkit.NamespacedKey;
+
+/**
+ * the {@link KeyManager} for default Minecraft
+ */
+public class MinecraftKeyManager extends AbstractKeyManager {
+  @Override
+  public NamespacedKey newKey(String key) {
+    return NamespacedKey.minecraft(key);
+  }
+}
